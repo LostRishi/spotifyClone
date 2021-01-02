9f11e753e7efe84b115b821f3fadae5ef31d4293
unknown <LOst>
2021-01-02 17:05:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react"
 import './App.css';
+import Login from "./Login"
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player"
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();  //allow spotify to talk to react
 
 function App() {
+  const [{ user, token }, dispatch] = useDataLayerValue(); //dataLayer.user  // pulling user from dataLayer
+
+  //run code based on some given condition 
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      })
+      spotify.setAccessToken(_token); //giving access token to spotify api
+      spotify.getMe().then(user => {  //getUsers account
+
+        dispatch({
+          type: "SET_USER",
+          user: user
+        });
+      });
+    }
+    //get data from spotify to show playlists
+    spotify.getUserPlaylists().then((playlists) => {
+      dispatch({
+        type: "SET_PLAYLISTS",
+        playlists: playlists
+      });
+    });
+    // console.log("I have a token >>>", token);
+
+    spotify.getPlaylist("4bu2M0DVRWmRoCwKZ47DWG").then(response =>
+      dispatch({
+        type: "SET_DISCOVER_WEEKLY",
+        discover_weekly: response
+      }));
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        token ? <Player spotify={spotify} /> : <Login />
+      }
     </div>
   );
 }
